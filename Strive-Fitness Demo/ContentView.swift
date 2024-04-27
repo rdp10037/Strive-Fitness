1d19b8a5b8023f8b16417e0b26f62ef4b36bdeee
rdp10037 <168250338+rdp10037@users.noreply.github.com>
2024-04-26 23:54:38 -0400

    initial dashboard ui

--- Strive-Fitness Demo/ContentView.swift
+++ Strive-Fitness Demo/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                VStack (spacing: 20){
+                    // Card section
+                    VStack {
+                        HStack {
+                            VStack (alignment: .leading){
+                                Label("Steps", systemImage: "figure.walk")
+                                    .font(.title3.bold())
+                                    .foregroundStyle(.pink)
+                                Text("Avg 10K steps")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundStyle(.secondary)
+                        }
+                        .padding(.bottom)
+                        
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 150)
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 12).fill(.ultraThinMaterial))
+                    
+                    
+                    // Card section
+                    VStack (alignment: .leading){
+                        VStack (alignment: .leading){
+                            Label("Averages", systemImage: "calendar")
+                                .font(.title3.bold())
+                                .foregroundStyle(.orange)
+                            Text("Last 28 Days")
+                                .font(.caption)
+                                .foregroundStyle(.secondary)
+                        }
+                        .padding(.bottom, 12)
+                        
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 240)
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 12).fill(.ultraThinMaterial))
+                }
+                .padding()
+            }
+            .navigationTitle("Dashboard")
+            .scrollIndicators(.hidden)
         }
-        .padding()
     }
 }
 
