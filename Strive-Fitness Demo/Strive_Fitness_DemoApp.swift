f2f0c9f62ed53c55d165c63f72a4a9f88a24f880
rdp10037 <168250338+rdp10037@users.noreply.github.com>
2024-04-26 23:08:02 -0400

    created xcode project

---
+++ Strive-Fitness Demo/Strive_Fitness_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Strive_Fitness_DemoApp.swift
+//  Strive-Fitness Demo
+//
+//  Created by Rob Pee on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Strive_Fitness_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
